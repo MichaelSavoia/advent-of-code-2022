1bc4981f8bf36081a3ade614a0ce425c6ec5dda9
Michael Savoia <msavoiadev@gmail.com>
2022-12-06 10:36:56 -0500

    day 4 solution

--- src/day-4/index.test.ts
+++ src/day-4/index.test.ts
@@ -1,1 +1,18 @@
-test.todo("TODO");
+import { solution1, solution2 } from "./";
+
+const input = `
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+`;
+
+test("solution1", () => {
+	expect(solution1(input)).toEqual(2);
+});
+
+test("solution2", () => {
+	expect(solution2(input)).toEqual(4);
+});
--- src/day-4/index.ts
+++ src/day-4/index.ts
@@ -1,3 +1,57 @@
 import { getInput } from "../utils";
 
 const data = getInput(__dirname);
+
+class Assignment {
+	start;
+	end;
+
+	constructor(start: number, end: number) {
+		this.start = start;
+		this.end = end;
+	}
+
+	// For part 1
+	contains = (assignment: Assignment) => {
+		return this.start <= assignment.start && this.end >= assignment.end;
+	};
+
+	// For part 2
+	overlaps = (assignment: Assignment) => {
+		return this.start <= assignment.end && this.end >= assignment.start;
+	};
+}
+
+function getPairedAssignments(input: string): Assignment[][] {
+	return input
+		.trim()
+		.split("\n")
+		.map((pair) =>
+			pair.split(",").map((assignment) => {
+				const [start, end] = assignment
+					.split("-")
+					.map((strNum) => parseInt(strNum, 10));
+				return new Assignment(start, end);
+			})
+		);
+}
+
+// Part 1
+
+export function solution1(input: string) {
+	return getPairedAssignments(input)
+		.map(([a, b]) => a.contains(b) || b.contains(a))
+		.filter((contains) => contains).length;
+}
+
+// console.log(solution1(data));
+
+// Part 2
+
+export function solution2(input: string) {
+	return getPairedAssignments(input)
+		.map(([a, b]) => a.overlaps(b) || b.overlaps(a))
+		.filter((contains) => contains).length;
+}
+
+// console.log(solution2(data));
