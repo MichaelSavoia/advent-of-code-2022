f2840372d58925cb883c996b7e06c2bc0bd1442d
Michael Savoia <msavoiadev@gmail.com>
2022-12-07 11:47:31 -0500

    day 6 solution

--- src/day-6/index.test.ts
+++ src/day-6/index.test.ts
@@ -1,1 +1,23 @@
-test.todo("TODO");
+import { solution1, solution2 } from "./";
+
+const input1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+const input2 = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+const input3 = "nppdvjthqldpwncqszvftbrmjlhg";
+const input4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+const input5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+
+test("solution1", () => {
+	expect(solution1(input1)).toBe(7);
+	expect(solution1(input2)).toBe(5);
+	expect(solution1(input3)).toBe(6);
+	expect(solution1(input4)).toBe(10);
+	expect(solution1(input5)).toBe(11);
+});
+
+test("solution2", () => {
+	expect(solution2(input1)).toBe(19);
+	expect(solution2(input2)).toBe(23);
+	expect(solution2(input3)).toBe(23);
+	expect(solution2(input4)).toBe(29);
+	expect(solution2(input5)).toBe(26);
+});
--- src/day-6/index.ts
+++ src/day-6/index.ts
@@ -1,3 +1,48 @@
 import { getInput } from "../utils";
 
 const data = getInput(__dirname);
+
+// Same function is used for both parts, the only difference is the number of
+// unique characters is required to qualify as a mark
+
+function getMarkerAfterUniqueSequence(
+	input: string,
+	uniqueSequenceLength: number
+) {
+	const characters = input.split("");
+
+	let indicators: string[] = [];
+	let i = 0;
+
+	while (indicators.length < uniqueSequenceLength) {
+		for (let j = i; j < i + uniqueSequenceLength; j++) {
+			const char = characters[j];
+
+			if (indicators.includes(char)) {
+				indicators = [];
+				break;
+			}
+
+			indicators.push(char);
+		}
+		i++;
+	}
+
+	return i + uniqueSequenceLength - 1;
+}
+
+// Part 1
+
+export function solution1(input: string) {
+	return getMarkerAfterUniqueSequence(input, 4);
+}
+
+// console.log(solution1(data));
+
+// Part 2
+
+export function solution2(input: string) {
+	return getMarkerAfterUniqueSequence(input, 14);
+}
+
+// console.log(solution2(data));
